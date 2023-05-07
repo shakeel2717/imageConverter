a4090467316c49763a3f40c6627cd12810c7fb1b
Shakeel Ahmad <shakeel2717@gmail.com>
2023-05-07 18:41:19 +0500

    first commit

---
+++ resources/views/includes/nav.blade.php
@@ -0,0 +1,9 @@
+<nav id="navbar" class="navbar">
+    <ul>
+        <li><a href="{{ route('index') }}" class="active">Home</a></li>
+        <li><a href="#" class="active">Image to TEXT</a></li>
+        <li><a href="#" class="active">Image to MP3</a></li>
+        <li><a href="#" class="active">Image to Video</a></li>
+        <li><a class="get-a-quote" href="get-a-quote.html">Convert Image to Text</a></li>
+    </ul>
+</nav>
