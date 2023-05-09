4303346de8f0f2e231c4ac424460371406111be2
Shakeel Ahmad <shakeel2717@gmail.com>
2023-05-09 19:40:54 +0500

    User can Convert Image to Video

--- app/Http/Controllers/ImageConverterController.php
+++ app/Http/Controllers/ImageConverterController.php
@@ -42,6 +42,48 @@ class ImageConverterController extends Controller
         return back()->with('result', ['img' => $text['img'], 'audio' => $outputFile]);
     }
 
+
+    public function video()
+    {
+        return view('convert.video');
+    }
+
+
+    public function videoConvert(Request $request)
+    {
+        $text = $this->getText($request->file('img'));
+        $text['data'] = preg_replace("/\r|\n/", " ", $text['data']);
+        $outputImg = $text['img'];
+        $audioFolder = 'audio';
+        if (!file_exists($audioFolder)) {
+            mkdir($audioFolder, 0777, true);
+        }
+        $audioFile = $audioFolder . '/' . rand(000000, 999999) . '.wav';
+        exec('espeak-ng -p 75 -s 150 -w ' . $audioFile . ' "' . $text['data'] . '"');
+
+        // Use FFmpeg to generate the video
+        $ffmpegPath = 'ffmpeg'; // path to ffmpeg executable
+        $videoFolder = 'video';
+        if (!file_exists($videoFolder)) {
+            mkdir($videoFolder, 0777, true);
+        }
+        $videoName = $videoFolder . '/' . rand(000000, 999999) . '.mp4'; // name of output video
+        $backgroundColor = 'pink'; // background color of video
+        $text = $text['data']; // text to be displayed in video
+        $audioPath = $audioFile; // path to audio file
+
+        // Run the ffmpeg command to get the duration of the audio file
+        $duration_output = shell_exec("ffmpeg -i \"$audioPath\" 2>&1");
+        preg_match("/Duration: (.{2}):(.{2}):(.{2})/", $duration_output, $matches);
+
+        // Calculate the duration of the audio file in seconds
+        $duration_seconds = ($matches[1] * 3600) + ($matches[2] * 60) + $matches[3] + 3;
+
+        $cmd = "$ffmpegPath -y -f lavfi -i color=c=$backgroundColor:s=1920x1080:d=$duration_seconds -i $audioPath -filter_complex \"[0:v]drawtext=fontfile=/Windows/Fonts/arial.ttf:text='$text':fontcolor=white:fontsize=50:y=(h-text_h)/2:x=-200*t:box=1:boxcolor=black@0.5:boxborderw=5[v];[v][1:a]concat=n=1:v=1:a=1\" $videoName";
+        exec($cmd);
+        return back()->with('result', ['img' => $outputImg, 'video' => $videoName]);
+    }
+
     public function getText($img)
     {
         $validated = Validator::make(['img' => $img], [
---
+++ resources/views/convert/video.blade.php
@@ -0,0 +1,54 @@
+@extends('layout.app')
+@section('content')
+@if (session('result'))
+<section id="horizontal-pricing" class="horizontal-pricing pt-0">
+    <div class="container aos-init aos-animate" data-aos="fade-up">
+
+        <div class="section-header">
+            <span>Output Result</span>
+            <h2>Output Result</h2>
+        </div>
+
+        <div class="row gy-4 pricing-item aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+            <div class="col-lg-12 w-100 text-center">
+                <h3>Your Image: </h3>
+                <img src="{{ asset('text/') }}/{{ session('result')['img'] }}" alt="Uploaded Image">
+            </div>
+            <div class="col-lg-12 text-center">
+                <h3>Result</h3>
+                <video width="100%" height="auto" autoplay controls>
+                    <source src="{{ session('result')['video'] }}" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+        </div>
+    </div>
+</section>
+@endif
+<section id="service" class="services pt-0">
+
+    <div class="container" data-aos="fade-up">
+        <div class="section-header">
+            <span>Convert Image to VIDEO</span>
+            <h2>Convert Image to VIDEO</h2>
+            <div class="col-md-8 mx-auto">
+                <p>Experience Fast and Accurate Image Conversion with Our Top-Notch Technology</p>
+            </div>
+        </div>
+
+        <div class="row gy-4">
+            <div class="col-lg-4 col-md-6 mx-auto" data-aos="fade-up" data-aos-delay="100">
+                <form action="{{ route('img-to-video.request') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group">
+                        <label for="img">Upload your Image</label>
+                        <input type="file" name="img" id="img" class="form-control">
+                    </div>
+                    <div class="form-group mt-3 row">
+                        <button type="submit" class="btn btn-primary btn-lg btn-block">Generate VIDEO</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+</section>
+@endsection
